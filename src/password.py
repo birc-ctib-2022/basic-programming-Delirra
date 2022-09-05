96323b8e03efaf8fc10ea37e21efd604283e2c9c
Rasmus Freund <rasmus3141@gmail.com>
2022-09-05 14:53:39 +0200

    Updated password.py

--- src/password.py
+++ src/password.py
@@ -8,5 +8,26 @@ password = sys.argv[1]
 is_valid = False
 
 # Do all the requirement checks here.
+lower_chars = 0
+upper_chars = 0
+special_chars = 0
+min_length = False
+max_length = False
+
+for char in password:
+    if char.islower():
+        lower_chars += 1
+    elif char.isupper():
+        upper_chars += 1
+    elif char in "$#@":
+        special_chars += 1
+
+if len(password) >= 6:
+    min_length = True
+if len(password) <= 16:
+    max_length = True
+
+if lower_chars and upper_chars and special_chars and min_length and max_length:
+    is_valid = True
 
 print(is_valid)
